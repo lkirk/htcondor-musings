f1053d73e086c5cf1ff8160487418981a20a7609
lloydkirk <kirklloyd@gmail.com>
2023-03-25 14:11:51 -0500

    now to test a python script

---
+++ README.md
@@ -0,0 +1,6 @@
+# HTCondor tests and demos
+
+The only demo that currently exists in the repo is a docker demo. The docker
+demo can be executed by navigating to the submission directory and running
+`condor_submit docker.sub`. More about htcondor's docker implementation can be
+found [here](https://htcondor.readthedocs.io/en/latest/users-manual/docker-universe-applications.html)
--- docker/analysis.py
+++ docker/analysis.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 
 # test data created with the following commands:
@@ -5,4 +7,11 @@ import numpy as np
 # >>> import numpy as np
 # >>> np.savetxt('test.txt', np.vstack([np.random.uniform(size=(100)), np.random.uniform(size=100)]).T, delimiter='\t')
 
-print(np.loadtxt('test.txt').mean(1))
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('--input', required=True)
+arg_parser.add_argument('--output', required=True)
+args = arg_parser.parse_args()
+
+with open(args.output) as out_fh:
+    print(np.loadtxt(args.input).mean(1), file=out_fh)
